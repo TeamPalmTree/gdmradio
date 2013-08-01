8f776ee83e49382ea8c670e8ca86abb1b8be41be
Alex Groleau <alex@teampalmtree.com>
2013-08-01 06:01:47 -0400

    removed tpt and shared less

---
+++ views/welcome/index.php
@@ -0,0 +1,10 @@
+<div class="gdm_listen">
+    <a class="btn btn-large" href="http://perceive.gdmradio.com">
+        <img src="assets/img/play.png" height="32" width="32" />
+        LISTEN NOW
+    </a>
+    <a class="btn btn-large" href="http://icecast.gdmradio.com:8000/128.mp3">
+        <img src="assets/img/mp3.png" height="32" width="32" />
+        MP3 STREAM
+    </a>
+</div>
